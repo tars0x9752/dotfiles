b1b532593ada2a90ea1e78d45103edccc65c7a13
Naoki Aoyama <46079709+tars0x9752@users.noreply.github.com>
2021-11-16 20:53:30 +0900

    add ruby setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,6 +23,10 @@ curl https://get.volta.sh | bash
 volta install node
 volta install yarn
 
+# Ruby environment
+rbenv install 2.7.4
+rbenv global 2.7.4
+
 # git config (expect name)
 git config --global user.email '46079709+tars0x9752@users.noreply.github.com'
 git config --global core.autocrlf false
